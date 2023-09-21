364a421f5b4e9f5c0324df631156115b60bc9381
Michael Victor <radmakson@gmail.com>
2023-09-21 12:16:12 +0100

    init

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import Typed from 'typed.js';
 
 const typed = new Typed('#element', {
-  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  typeSpeed: 50,
+  strings: ['Lets Generate Some Quotes', 'Click the Generate Button to Start', 'Stay Inspired and be Motivated'],
+  typeSpeed: 90,
   backSpeed: 210,
   shuffle: false,
   smartBackspace: false,
