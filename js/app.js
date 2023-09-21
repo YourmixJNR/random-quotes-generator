a08547337377de807cca79bd934509e4a97a8d59
Michael Victor <radmakson@gmail.com>
2023-09-21 12:11:53 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+const typed = new Typed('#element', {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 50,
+  backSpeed: 210,
+  shuffle: false,
+  smartBackspace: false,
+  loop: true
+});
