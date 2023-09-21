f6fffd2050a24832abd92fc47da0c8803bd50cea
Michael Victor <radmakson@gmail.com>
2023-09-21 12:50:58 +0100

    header completed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 const typed = new Typed('#element', {
-  strings: ['Lets Generate Some Quotes', 'Click the Generate Button to Start', 'Stay Inspired and be Motivated'],
+  strings: ['Let\'\s Generate Some Quotes', 'Click the Generate Button to Start', 'Stay Inspired and be Motivated'],
   typeSpeed: 90,
   backSpeed: 210,
   shuffle: false,
